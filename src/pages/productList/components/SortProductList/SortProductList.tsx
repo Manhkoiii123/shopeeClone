05f3c35797d5a0d593bc7768d2a0e5448155861a
Tran Duc Manh <manhtranduc0202@gmail.com>
2023-09-06 18:48:31 +0700

    fix icons in productList

--- src/pages/productList/components/SortProductList/SortProductList.tsx
+++ src/pages/productList/components/SortProductList/SortProductList.tsx
@@ -144,11 +144,11 @@ export default function SortProductList({ queryConfig, pageSize }: Props) {
                   xmlns='http://www.w3.org/2000/svg'
                   fill='none'
                   viewBox='0 0 24 24'
-                  strokeWidth={1.5}
+                  strokeWidth='1.5'
                   stroke='currentColor'
                   className='w-3 h-3'
                 >
-                  <path strokeLinecap='round' strokeLinejoin='round' d='M15.75 19.5L8.25 12l7.5-7.5' />
+                  <path strokeLinecap='round' strokeLinejoin='round' d='M8.25 4.5l7.5 7.5-7.5 7.5' />
                 </svg>
               </span>
             ) : (
